9dc8bb7f15dfeff2fcc2731a25d36fe4d85593ca
ADITIVASUDEVA123 <aditivasudeva2002@gmail.com>
2024-10-26 22:47:25 +0530

    requirements added

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import find_packages, setup
+
+setup(
+    name = 'WasteDetection',
+    version= '0.0.0',
+    author= 'Aditi Vasudeva ; Ananya Gaur',
+    author_email= 'aditivasudeva2002@gmail.com ; hiananya02@gmail.com',
+    packages= find_packages(),
+    install_requires = []
+
+)
