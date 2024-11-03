b725cb22001969a2231316636e7489f3e134ab94
hiananya02@gmail.com <hiananya02@gmail.com>
2024-11-03 23:59:04 +0530

    data ingestion added

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # End-to-End-Waste-Detection-using-YOLO-v5
+## Workflows -> update in following order
+1. constants
+2. entity
+3. components
+4. pipelines
+5. app.py
---
+++ app.py
@@ -0,0 +1,8 @@
+from wasteDetection.logger import logging
+
+from wasteDetection.exception import AppException
+import sys
+from wasteDetection.pipeline.training_pipeline import TrainPipeline
+
+obj = TrainPipeline()
+obj.run_pipeline()
