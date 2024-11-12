e146c47991b796daf0a59d662a37a43f5cd0230c
ADITIVASUDEVA123 <aditivasudeva2002@gmail.com>
2024-11-12 17:32:06 +0530

    comment changes

--- wasteDetection/entity/config_entity.py
+++ wasteDetection/entity/config_entity.py
@@ -96,13 +96,13 @@ class DataValidationConfig:
 
 
 # ModelTrainerConfig component - commented out for future use
-@dataclass
-class ModelTrainerConfig:
-    model_trainer_dir: str = os.path.join(
-        training_pipeline_config.artifacts_dir, MODEL_TRAINER_DIR_NAME
-    )
+# @dataclass
+# class ModelTrainerConfig:
+#     model_trainer_dir: str = os.path.join(
+#         training_pipeline_config.artifacts_dir, MODEL_TRAINER_DIR_NAME
+#     )
 
-    weight_name: str = MODEL_TRAINER_PRETRAINED_WEIGHT_NAME
+#     weight_name: str = MODEL_TRAINER_PRETRAINED_WEIGHT_NAME
 
-    no_epochs: int = MODEL_TRAINER_NO_EPOCHS
-    batch_size: int = MODEL_TRAINER_BATCH_SIZE
+#     no_epochs: int = MODEL_TRAINER_NO_EPOCHS
+#     batch_size: int = MODEL_TRAINER_BATCH_SIZE
