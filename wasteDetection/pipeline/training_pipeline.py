dcff4bae8629ab491f0690c9ec610c03f3f88990
hiananya02@gmail.com <hiananya02@gmail.com>
2024-11-16 16:48:40 +0530

    model trainer added

--- wasteDetection/pipeline/training_pipeline.py
+++ wasteDetection/pipeline/training_pipeline.py
@@ -3,16 +3,16 @@ from wasteDetection.logger import logging
 from wasteDetection.exception import AppException
 from wasteDetection.components.data_ingestion import DataIngestion
 from wasteDetection.components.data_validation import DataValidation
-# from wasteDetection.components.model_trainer import ModelTrainer
+from wasteDetection.components.model_trainer import ModelTrainer
 
 
 from wasteDetection.entity.config_entity import (DataIngestionConfig ,DataValidationConfig
-                                                # , ModelTrainerConfig
+                                                , ModelTrainerConfig
                                                  )
 
 from wasteDetection.entity.artifacts_entity import (DataIngestionArtifact
                                                      ,DataValidationArtifact
-                                                    # , ModelTrainerArtifact
+                                                    , ModelTrainerArtifact
                                                     )
 
 
@@ -20,7 +20,7 @@ class TrainPipeline:
     def __init__(self):
         self.data_ingestion_config = DataIngestionConfig()
         self.data_validation_config = DataValidationConfig()
-        # self.model_trainer_config = ModelTrainerConfig()
+        self.model_trainer_config = ModelTrainerConfig()
 
 
     
@@ -76,17 +76,17 @@ class TrainPipeline:
 
 
     
-    # def start_model_trainer(self
-    #  ) -> ModelTrainerArtifact:
-    #     try:
-    #          model_trainer = ModelTrainer(
-    #             model_trainer_config=self.model_trainer_config,
-    #          )
-    #          model_trainer_artifact = model_trainer.initiate_model_trainer()
-    #          return model_trainer_artifact
+    def start_model_trainer(self
+     ) -> ModelTrainerArtifact:
+        try:
+             model_trainer = ModelTrainer(
+                model_trainer_config=self.model_trainer_config,
+             )
+             model_trainer_artifact = model_trainer.initiate_model_trainer()
+             return model_trainer_artifact
 
-    #     except Exception as e:
-    #      raise AppException(e, sys)
+        except Exception as e:
+         raise AppException(e, sys)
         
         
 
@@ -101,11 +101,11 @@ class TrainPipeline:
                  data_ingestion_artifact=data_ingestion_artifact
              )
 
-            # if data_validation_artifact.validation_status == True:
-            #      model_trainer_artifact = self.start_model_trainer()
+            if data_validation_artifact.validation_status == True:
+                 model_trainer_artifact = self.start_model_trainer()
             
-            # else:
-            #      raise Exception("Your data is not in correct format")
+            else:
+                 raise Exception("Your data is not in correct format")
 
         
         except Exception as e:
